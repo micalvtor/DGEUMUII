b777f5b20ed7030ff5f6d3a60b95e22a35c42282
micalvtor <45856194+micalvtor@users.noreply.github.com>
2020-05-07 12:33:26 +0200

    Update Dropdown2.tsx

--- src/container/Dropdown2.tsx
+++ src/container/Dropdown2.tsx
@@ -1,16 +1,11 @@
+import "../assets/scss/Dropdown.scss"; 
+import IGlobalState from '../state/globalState';
 import Dropdown2 from '../components/Dropdown2';
 import {connect} from 'react-redux';
-import IGlobalState from '../state/globalState';
-import {Dispatch} from 'redux';
+
 const mapStateToProps=(state:IGlobalState) =>({
     displayFinal:state.displayFinal,
+
 })
 
-// Para realizar la creación de esto hay que hacerlo mediante map
-const mapDispatchToProps=(dispatch:Dispatch) =>({
-    showDropdownMenuResponsive:(displayFinal:boolean, index: number) =>{
-    dispatch({type:"DISPLAYMENURESPONSIVE",payload:{displayFinal,index}});
-}
-})
-
-export default connect(mapStateToProps,mapDispatchToProps)(Dropdown2);
+export default connect(mapStateToProps)(Dropdown2);
