7d7650c26534ec46da33c30dc6a7ce89a516e89f
micalvtor <45856194+micalvtor@users.noreply.github.com>
2020-08-24 09:21:27 +0200

    Add files via upload

---
+++ build/precache-manifest.0d5da20929df23bacb13c0e677ef9889.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d04e43f867413f0c6083",
+    "url": "/static/js/main.d04e43f8.chunk.js"
+  },
+  {
+    "revision": "697e74ea0dc4b1341691",
+    "url": "/static/js/1.697e74ea.chunk.js"
+  },
+  {
+    "revision": "d04e43f867413f0c6083",
+    "url": "/static/css/main.9d9737fc.chunk.css"
+  },
+  {
+    "revision": "dcbdd2e576ae94ce0794a4fa911ab6cb",
+    "url": "/index.html"
+  }
+];
