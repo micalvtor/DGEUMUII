95d6c3bc02a5de3b53ff9a2dbc7be140be6ae329
micalvtor <45856194+micalvtor@users.noreply.github.com>
2020-08-27 12:59:49 +0200

    Update Dropdown2.tsx

--- src/components/Dropdown2.tsx
+++ src/components/Dropdown2.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
-import "../assets/scss/Dropdown.scss";  
+import "../assets/scss/Dropdown.scss"; 
+import { createUseStyles, useTheme } from "react-jss"; 
 import {IMenuItem} from "../state/globalState";
 import MenuItemResponsive from '../components/MenuItemResponsive';
 
@@ -10,6 +11,22 @@ interface IHeaderState{
   displayFinal: IMenuItem[];
 
 }
+const useStyles = createUseStyles({
+  maquetacion: {
+    backgroundColor: "#981E32",
+    border:"3px solid #FDC82F",
+    color: "#FDC82F",
+    fontFamily:  "Times New Roman",
+    fontSize: "110%",
+    marginRight:"-10%",
+    maxHeight:20,
+    maxWidth:160,
+    minWidth:160,
+    textAlign: "center",
+    verticalAlign: "top",
+    
+  }
+}) 
 class Dropdown extends React.Component<IHeaderProps,IHeaderState> {
 constructor(props:IHeaderProps){
  super(props);
@@ -21,13 +38,14 @@ public render() {
     const menuGroup = this.props.displayFinal.map((menuItems:IMenuItem, index:number) => {
       return (<MenuItemResponsive key={index}  desplegable={menuItems.desplegable} factores={menuItems.factores} seccion={menuItems.seccion}/>);
     })
-    
+    const CompResponsive = () => {
+      const theme = useTheme();
+      const maquetacion = useStyles({ theme });
+      return(<div className={maquetacion.maquetacion}>{menuGroup}</div>)
+    }
       return (
         <div className="topnav" id="menú">
-        Menú
-<div id="subMenú">
-{menuGroup}
-</div>
+        <CompResponsive />
 </div>
 
   );
